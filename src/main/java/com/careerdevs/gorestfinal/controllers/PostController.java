11209b175e2a0cddf8c3a0f79697c5ae5143a016
MayraMendoza <mayra601m@gmail.com>
2022-04-25 17:21:22 -0400

    Comment/ post progress

--- src/main/java/com/careerdevs/gorestfinal/controllers/PostController.java
+++ src/main/java/com/careerdevs/gorestfinal/controllers/PostController.java
@@ -6,11 +6,16 @@ import com.careerdevs.gorestfinal.utils.ApiErrorHandling;
 import com.careerdevs.gorestfinal.validation.PostValidation;
 import com.careerdevs.gorestfinal.validation.ValidationError;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.Optional;
 
 
@@ -18,19 +23,19 @@ import java.util.Optional;
       Required Routes for GoRestSQL Final: complete for each resource; User, Post, Comment, Todo,
 
            * * GET route that returns one [resource] by ID from the SQL database
-           * GET route that returns all [resource]s stored in the SQL database
+           **GET route that returns all [resource]s stored in the SQL database
            ** DELETE route that deletes one [resource] by ID from SQL database (returns the deleted SQL [resource] data)
-           * DELETE route that deletes all [resource]s from SQL database (returns how many [resource]s were deleted)
-           * POST route that queries one [resource] by ID from GoREST and saves their data to your local database (returns
+           ** DELETE route that deletes all [resource]s from SQL database (returns how many [resource]s were deleted)
+           ** POST route that queries one [resource] by ID from GoREST and saves their data to your local database (returns
            the SQL [resource] data)
-           *POST route that uploads all [resource]s from the GoREST API into the SQL database (returns how many
+           **POST route that uploads all [resource]s from the GoREST API into the SQL database (returns how many
            [resource]s were uploaded)
-           *POST route that create a [resource] on JUST the SQL database (returns the newly created SQL [resource] data)
+           **POST route that create a [resource] on JUST the SQL database (returns the newly created SQL [resource] data)
            *PUT route that updates a [resource] on JUST the SQL database (returns the updated SQL [resource] data)
     * */
 
 @RestController
-@RequestMapping("/api/posts")
+@RequestMapping("/posts")
 public class PostController {
 
     @Autowired
@@ -40,6 +45,8 @@ public class PostController {
     public String testRoute(){
         return "Testing!";
     }
+
+
     //* GET route that returns one [resource] by ID from the SQL database
 
     @GetMapping("/{id}")
@@ -108,9 +115,96 @@ public class PostController {
 
     //* DELETE route that deletes all [resource]s from SQL database (returns how many [resource]s were deleted)
     @DeleteMapping("/all")
-    public ResponseEntity<?> deleteAllPosts(@PathVariable)
+    public ResponseEntity<?> deleteAllPosts(){
+        try{
+            long totalPosts = postRepository.count();
+            postRepository.deleteAll();
+            return new ResponseEntity<>("Posts deleted: "+ totalPosts , HttpStatus.OK );
+        } catch (HttpClientErrorException e) {
+            return ApiErrorHandling.customApiError(e.getMessage(), e.getStatusCode());
+        }catch (Exception e){
+            return ApiErrorHandling.genericApiError(e);
+        }
+
+    }
+
+    //* POST route that queries one [resource] by ID from GoREST and saves their data to your local database (returns the SQL [resource] data)
+
+    @PostMapping("/upload/{id}")
+    public ResponseEntity<?> postById (@PathVariable("id") String postId,
+                                       RestTemplate restTemplate){
+        try {
+            if (ApiErrorHandling.isStrNan(postId)) { // check if post id is a number and is not empty. if it is throw Http Error
+                throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, postId + " is not a valid id");
+
+            }
+
+            long uID = Integer.parseInt(postId);
+
+            String url = "https://gorest.co.in/public/v2/posts/" + uID;
+            Post foundPost = restTemplate.getForObject(url, Post.class);
+            System.out.println("found post");
+            System.out.println(foundPost);
+
+            if (foundPost == null) {
+                throw new HttpClientErrorException(HttpStatus.NOT_FOUND, "user data was null");
+
+            }
+            // update post / SAVE POST
+            Post savePost = postRepository.save(foundPost);
+
+            return new ResponseEntity<>(savePost, HttpStatus.CREATED);
+
+        }catch (Exception e){
+            return ApiErrorHandling.genericApiError(e);
 
 
+        }
+    }
+
+    //*POST route that uploads all [resource]s from the GoREST API into the SQL database (returns how many[resource]s were uploaded)
+
+    @PostMapping("/uploadAll")
+    public ResponseEntity uploadAll (RestTemplate restTemplate){
+        try{
+            // rest api url
+            String url = "https://gorest.co.in/public/v2/posts";
+
+            ResponseEntity<Post[]> response = restTemplate.getForEntity(url, Post[].class);
+            Post[] firstPagePost = response.getBody();
+            assert firstPagePost != null;
+            if(firstPagePost == null){
+                throw new HttpClientErrorException(HttpStatus.INTERNAL_SERVER_ERROR, " Failed to get first page of go rest post");
+
+            }
+
+            ArrayList<Post> allPosts = new ArrayList<>(Arrays.asList(firstPagePost));
+
+            HttpHeaders resposeHeader = response.getHeaders();
+            String totalPages = Objects.requireNonNull(resposeHeader.get("X-Pagination-Pages").get(0));
+
+            int totalPgNum =Integer.parseInt(totalPages);
+
+            for( int i = 2; i<= totalPgNum; i++) {
+                String tempUrl = url + "?=page=" + i;
+                Post[] pagePost = restTemplate.getForObject(tempUrl, Post[].class);
+                if (pagePost == null) {
+                    throw new HttpClientErrorException(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to get page " + i + "of posts");
+                }
+                allPosts.addAll(Arrays.asList(firstPagePost));
+            }
+
+            postRepository.saveAll(allPosts);
+
+            return new ResponseEntity("posts added " + allPosts.size(),HttpStatus.OK);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            System.out.println(e.getMessage());
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+//*POST route that create a [resource] on JUST the SQL database (returns the newly created SQL [resource] data)
     @PostMapping("//")
     public ResponseEntity<?> createPost(@RequestBody Post newPost){
         try{
@@ -126,4 +220,23 @@ public class PostController {
            return ApiErrorHandling.genericApiError(e);
         }
     }
+
+    // *PUT route that updates a [resource] on JUST the SQL database (returns the updated SQL [resource] data)
+    @PutMapping("/")
+    public ResponseEntity<?> updatingPost(@RequestBody Post updatePost){
+        try{
+            ValidationError newPostErrors = PostValidation.validatePost(updatePost,postRepository,true );
+            if(newPostErrors.hasError()){
+                throw  new HttpClientErrorException(HttpStatus.BAD_REQUEST, newPostErrors.toString());
+
+            }
+            Post savedPost = postRepository.save(updatePost);
+            return  new ResponseEntity<>(savedPost, HttpStatus.OK);
+        }catch (HttpClientErrorException e){
+            return ApiErrorHandling.customApiError(e.getMessage(), e.getStatusCode());
+        }catch (Exception e){
+            return ApiErrorHandling.genericApiError(e);
+        }
+    }
+
 }
